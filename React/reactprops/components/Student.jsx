4171b11364bb7587909b61e7419221c666556fbe
theskysid <23x10sid@gmail.com>
2025-11-03 09:43:14 +0530

    React props

--- React/reactprops/components/Student.jsx
+++ React/reactprops/components/Student.jsx
@@ -1,15 +1,16 @@
 import React from 'react'
 import './Student.css'
 
-function Student(props) {
+function Student({data}) {
   return (
     <div className='container'>
        <h1>Student Info</h1>
-       <h2>Name: {props.name}</h2>
-       <h2>Roll No: {props.roll}</h2>
-       <h2>Branch: {props.branch}</h2>
-       <h2>Section: {props.section}</h2>
-       <h2>College: {props.college}</h2>
+       <img src={data.pic} alt="profile page" height={200} width={200} />
+       <h2>Name: {data.name}</h2>
+       <h2>Roll No: {data.roll}</h2>
+       <h2>Branch: {data.branch}</h2>
+       <h2>Section: {data.section}</h2>
+       <h2>College: {data.college}</h2>
     </div>
   )
 }
--- React/reactprops/src/App.jsx
+++ React/reactprops/src/App.jsx
@@ -1,9 +1,48 @@
 import Student from '../components/Student.jsx'
 
 function App() {
+
+const student=[{
+  pic:`https://avatars.githubusercontent.com/u/104955330?v=4`,
+  name : "Siddhant Rastogi",
+  roll:"182",
+  branch:"CSE AIML",
+  section:"B",
+  college:"ABES Engineering College"
+},
+{pic:`https://avatars.githubusercontent.com/u/104955330?v=4`,
+  name : "Shaurya Sharma",
+  roll:"172",
+  branch:"CSE AIML",
+  section:"B",
+  college:"ABES Engineering College"
+},
+{pic:`https://avatars.githubusercontent.com/u/104955330?v=4`,
+  name : "Yash Kumar",
+  roll:"202",
+  branch:"CSE AIML",
+  section:"B",
+  college:"ABES Engineering College"
+},
+{pic:`https://avatars.githubusercontent.com/u/104955330?v=4`,
+  name : "Raman Grewal",
+  roll:"182",
+  branch:"CSE AIML",
+  section:"B",
+  college:"ABES Engineering College"
+}]
+
   return (
     <div>
-      <Student name = "Siddhant Rastogi" roll="182" branch="CSE AIML" section="B" college="ABES Engineering College"/>
+      {/* Method 1 <Student name = "Siddhant Rastogi" roll="182" branch="CSE AIML" section="B" college="ABES Engineering College"/> */}
+      
+      {/* Method 2 for single object <Student data={student} /> */}
+
+      {
+        student.map((element, index)=>(
+          <Student data={element} key={index}/>
+        ))
+      }
     </div>
   )
 }
